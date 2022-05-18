915722efc54af61bb064fa73f3c6df89d80f2955
Bruna Araújo Almeida <CIANDT\brunaaraujo@lnb036090cps.ciandt.global>
2022-05-18 01:58:51 -0300

    Semana 1 - [Favorecendo encapsulamento na classe Pedido] e [Extrair processamento de CSV da classe Main]

--- src/main/java/br/com/alura/comex/Main.java
+++ src/main/java/br/com/alura/comex/Main.java
@@ -1,93 +1,100 @@
 package br.com.alura.comex;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        ArrayList<Pedido> pedidos = new ArrayList<>();
+	public static void main(String[] args) throws IOException, URISyntaxException {
 
-        try {
-            URL recursoCSV = ClassLoader.getSystemResource("pedidos.csv");
-            Path caminhoDoArquivo = caminhoDoArquivo = Path.of(recursoCSV.toURI());
+		Pedido pedido = new Pedido();
+		ArrayList<Pedido> pedidos = new ProcessadorDeCsv().registrarPedidos();
 
-            Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
+		/*
+		 * try {
+		 * 
+		 * URL recursoCSV = ClassLoader.getSystemResource("pedidos.csv"); Path
+		 * caminhoDoArquivo = caminhoDoArquivo = Path.of(recursoCSV.toURI());
+		 * 
+		 * Scanner leitorDeLinhas = new Scanner(caminhoDoArquivo);
+		 * 
+		 * leitorDeLinhas.nextLine();
+		 * 
+		 * int quantidadeDeRegistros = 0; while (leitorDeLinhas.hasNextLine()) { String
+		 * linha = leitorDeLinhas.nextLine(); String[] registro = linha.split(",");
+		 * 
+		 * String categoria = registro[0]; String produto = registro[1]; BigDecimal
+		 * preco = new BigDecimal(registro[2]); int quantidade =
+		 * Integer.parseInt(registro[3]); LocalDate data = LocalDate.parse(registro[4],
+		 * DateTimeFormatter.ofPattern("dd/MM/yyyy")); String cliente = registro[5];
+		 * 
+		 * Pedido pedido = new Pedido(categoria, produto, cliente, preco, quantidade,
+		 * data); pedidos.add(pedido);
+		 * 
+		 * quantidadeDeRegistros++; }
+		 * 
+		 * } catch (URISyntaxException e) { throw new
+		 * RuntimeException("Arquivo pedido.csv não localizado!"); } catch (IOException
+		 * e) { throw new
+		 * RuntimeException("Erro ao abrir Scanner para processar arquivo!"); }
+		 */
 
-            leitorDeLinhas.nextLine();
+		int totalDeProdutosVendidos = 0;
+		int totalDePedidosRealizados = 0;
+		/*
+		 * BigDecimal montanteDeVendas = BigDecimal.ZERO; Pedido pedidoMaisBarato =
+		 * null; Pedido pedidoMaisCaro = null;
+		 */
 
-            int quantidadeDeRegistros = 0;
-            while (leitorDeLinhas.hasNextLine()) {
-                String linha = leitorDeLinhas.nextLine();
-                String[] registro = linha.split(",");
+		CategoriasProcessadas categoriasProcessadas = new CategoriasProcessadas();
+		int totalDeCategorias = 0;
 
-                String categoria = registro[0];
-                String produto = registro[1];
-                BigDecimal preco = new BigDecimal(registro[2]);
-                int quantidade = Integer.parseInt(registro[3]);
-                LocalDate data = LocalDate.parse(registro[4], DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                String cliente = registro[5];
+		for (int i = 0; i < pedidos.size(); i++) {
+			Pedido pedidoAtual = pedidos.get(i);
 
-                Pedido pedido = new Pedido(categoria, produto, cliente, preco, quantidade, data);
-                pedidos.add(pedido);
+			if (pedidoAtual == null)
+				break;
 
-                quantidadeDeRegistros++;
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Arquivo pedido.csv não localizado!");
-        } catch (IOException e) {
-            throw new RuntimeException("Erro ao abrir Scanner para processar arquivo!");
-        }
+			/*
+			 * if (pedidoMaisBarato == null || pedidoAtual.getPreco().multiply(new
+			 * BigDecimal(pedidoAtual.getQuantidade()))
+			 * .compareTo(pedidoMaisBarato.getPreco() .multiply(new
+			 * BigDecimal(pedidoMaisBarato.getQuantidade()))) < 0) { pedidoMaisBarato =
+			 * pedidoAtual; }
+			 */
 
+			pedido.isMaisBaratoQue(pedidoAtual);
+			pedido.isMaisCaroQue(pedidoAtual);
 
-        int totalDeProdutosVendidos = 0;
-        int totalDePedidosRealizados = 0;
-        BigDecimal montanteDeVendas = BigDecimal.ZERO;
-        Pedido pedidoMaisBarato = null;
-        Pedido pedidoMaisCaro = null;
+			/*
+			 * if (pedidoMaisCaro == null || pedidoAtual.getPreco().multiply(new
+			 * BigDecimal(pedidoAtual.getQuantidade())).compareTo(
+			 * pedidoMaisCaro.getPreco().multiply(new
+			 * BigDecimal(pedidoMaisCaro.getQuantidade()))) > 0) { pedidoMaisCaro =
+			 * pedidoAtual; }
+			 */
 
-        CategoriasProcessadas categoriasProcessadas = new CategoriasProcessadas();
-        int totalDeCategorias = 0;
+			pedido.setValorTotal(pedidoAtual);
 
-        for (int i = 0; i < pedidos.size(); i++) {
-            Pedido pedidoAtual = pedidos.get(i);
+			totalDeProdutosVendidos += pedidoAtual.getQuantidade();
+			totalDePedidosRealizados++;
 
-            if (pedidoAtual == null) {
-                break;
-            }
+			// categoria
+			if (!categoriasProcessadas.contains(pedidoAtual.getCategoria())) {
+				totalDeCategorias++;
+				categoriasProcessadas.add(pedidoAtual.getCategoria());
+			}
+		}
 
-            if (pedidoMaisBarato == null || pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(pedidoMaisBarato.getPreco().multiply(new BigDecimal(pedidoMaisBarato.getQuantidade()))) < 0) {
-                pedidoMaisBarato = pedidoAtual;
-            }
-
-            if (pedidoMaisCaro == null || pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(pedidoMaisCaro.getPreco().multiply(new BigDecimal(pedidoMaisCaro.getQuantidade()))) > 0) {
-                pedidoMaisCaro = pedidoAtual;
-            }
-
-            montanteDeVendas = montanteDeVendas.add(pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())));
-            totalDeProdutosVendidos += pedidoAtual.getQuantidade();
-            totalDePedidosRealizados++;
-
-            if (!categoriasProcessadas.contains(pedidoAtual.getCategoria())) {
-              totalDeCategorias++;
-              categoriasProcessadas.add(pedidoAtual.getCategoria());
-            }
-        }
-
-        System.out.println("#### RELATÓRIO DE VALORES TOTAIS");
-        System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", totalDePedidosRealizados);
-        System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", totalDeProdutosVendidos);
-        System.out.printf("- TOTAL DE CATEGORIAS: %s\n", totalDeCategorias);
-        System.out.printf("- MONTANTE DE VENDAS: %s\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(montanteDeVendas.setScale(2, RoundingMode.HALF_DOWN)));
-        System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(pedidoMaisBarato.getPreco().multiply(new BigDecimal(pedidoMaisBarato.getQuantidade())).setScale(2, RoundingMode.HALF_DOWN)), pedidoMaisBarato.getProduto());
-        System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(pedidoMaisCaro.getPreco().multiply(new BigDecimal(pedidoMaisCaro.getQuantidade())).setScale(2, RoundingMode.HALF_DOWN)), pedidoMaisCaro.getProduto());
-    }
+		System.out.println("#### RELATÓRIO DE VALORES TOTAIS");
+		System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", totalDePedidosRealizados);
+		System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", totalDeProdutosVendidos);
+		System.out.printf("- TOTAL DE CATEGORIAS: %s\n", totalDeCategorias);
+		System.out.printf("- MONTANTE DE VENDAS: %s\n", pedido.getValorTotal());
+		System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", pedido.getMaisBaratoQue(),
+				pedido.getPedidoMaisBarato().getProduto());
+		System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", pedido.getMaisCaroQue(),
+				pedido.getPedidoMaisCaro().getProduto());
+	}
 }
--- src/main/java/br/com/alura/comex/Pedido.java
+++ src/main/java/br/com/alura/comex/Pedido.java
@@ -1,62 +1,116 @@
 package br.com.alura.comex;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Pedido {
 
-    private String categoria;
-    private String produto;
-    private String cliente;
+	private String categoria;
+	private String produto;
+	private String cliente;
 
-    private BigDecimal preco;
-    private int quantidade;
+	private BigDecimal preco;
+	private int quantidade;
 
-    private LocalDate data;
+	private BigDecimal montanteDeVendas = BigDecimal.ZERO;
+	private Pedido pedidoMaisBarato = null;
+	private Pedido pedidoMaisCaro = null;
 
-    public Pedido(String categoria, String produto, String cliente, BigDecimal preco, int quantidade, LocalDate data) {
-        this.categoria = categoria;
-        this.produto = produto;
-        this.cliente = cliente;
-        this.preco = preco;
-        this.quantidade = quantidade;
-        this.data = data;
-    }
+	private LocalDate data;
 
-    public String getCategoria() {
-        return categoria;
-    }
+	public Pedido() {
+	}
 
-    public String getProduto() {
-        return produto;
-    }
+	public Pedido(String categoria, String produto, String cliente, BigDecimal preco, int quantidade, LocalDate data) {
+		this.categoria = categoria;
+		this.produto = produto;
+		this.cliente = cliente;
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
 
-    public String getCliente() {
-        return cliente;
-    }
+	public boolean isMaisBaratoQue(Pedido pedidoAtual) {
+		if (this.pedidoMaisBarato == null || pedidoAtual.getPreco()
+				.multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(this.pedidoMaisBarato.getPreco()
+						.multiply(new BigDecimal(this.pedidoMaisBarato.getQuantidade()))) < 0) {
 
-    public BigDecimal getPreco() {
-        return preco;
-    }
+			this.pedidoMaisBarato = pedidoAtual;
+			return true;
+		}
+		return false;
+	}
 
-    public int getQuantidade() {
-        return quantidade;
-    }
+	public boolean isMaisCaroQue(Pedido pedidoAtual) {
+		if (pedidoMaisCaro == null
+				|| pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(
+						pedidoMaisCaro.getPreco().multiply(new BigDecimal(this.pedidoMaisCaro.getQuantidade()))) > 0) {
 
-    public LocalDate getData() {
-        return data;
-    }
+			this.pedidoMaisCaro = pedidoAtual;
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public String toString() {
-        return "Pedido{" +
-                "categoria='" + categoria + '\'' +
-                ", produto='" + produto + '\'' +
-                ", cliente='" + cliente + '\'' +
-                ", preco=" + preco +
-                ", quantidade=" + quantidade +
-                ", data=" + data +
-                '}';
-    }
+	public void setValorTotal(Pedido pedidoAtual) {
+		this.montanteDeVendas = this.montanteDeVendas
+				.add(pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())));
+	}
+
+	public String getValorTotal() {
+		return NumberFormat.getCurrencyInstance(new Locale("pt", "BR"))
+				.format(this.montanteDeVendas.setScale(2, RoundingMode.HALF_DOWN));	
+	}
+
+	public String getMaisBaratoQue() {
+		return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(pedidoMaisBarato.getPreco()
+				.multiply(new BigDecimal(pedidoMaisBarato.getQuantidade())).setScale(2, RoundingMode.HALF_DOWN));
+	}
+
+	public Pedido getPedidoMaisBarato() {
+		return this.pedidoMaisBarato;
+	}
+
+	public String getMaisCaroQue() {
+		return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(pedidoMaisCaro.getPreco()
+				.multiply(new BigDecimal(pedidoMaisCaro.getQuantidade())).setScale(2, RoundingMode.HALF_DOWN));
+	}
+	
+	public Pedido getPedidoMaisCaro() {
+		return this.pedidoMaisCaro;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public String getProduto() {
+		return produto;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido{" + "categoria='" + categoria + '\'' + ", produto='" + produto + '\'' + ", cliente='" + cliente
+				+ '\'' + ", preco=" + preco + ", quantidade=" + quantidade + ", data=" + data + '}';
+	}
 
 }
