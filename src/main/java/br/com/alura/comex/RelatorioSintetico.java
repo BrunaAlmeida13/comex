9a04999a8362c636a6766b523a1a845786c6a670
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-07-19 13:07:22 -0300

    [Refatoração de total de produtos vendidos no relatório sintético]

--- src/main/java/br/com/alura/comex/RelatorioSintetico.java
+++ src/main/java/br/com/alura/comex/RelatorioSintetico.java
@@ -1,7 +1,6 @@
 package br.com.alura.comex;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashSet;
 
 import br.com.alura.comex.pedido.CalculosDosPedidos;
@@ -13,32 +12,28 @@ public class RelatorioSintetico {
 	Pedido minValor = new Pedido();
 	CalculosDosPedidos calculosDosPedidos = new CalculosDosPedidos();
 
-	private int totalDeProdutosVendidos = 0;
 	private int totalDeCategorias = 0;
 
 	public RelatorioSintetico() throws NoSuchFieldException {
-		this.imprimeRelatorio(); 
+		this.imprimeRelatorio();
 	}
-	
+
 	private void geraRelatorio() throws NoSuchFieldException {
 		ArrayList<Pedido> pedidos = new ProcessadorDeCsv().registrarPedidos();
 
-
-
 		HashSet<String> categoriasProcessadas = new HashSet<String>();
+
 		calculosDosPedidos.calcularMontante(pedidos);
 		calculosDosPedidos.definirMaisBarato(pedidos);
 		calculosDosPedidos.definirMaisCaro(pedidos);
+		calculosDosPedidos.contarTotalProdutoVendidos(pedidos);
+
 		for (int i = 0; i < pedidos.size(); i++) {
 			Pedido pedidoAtual = pedidos.get(i);
 
 			if (pedidoAtual == null)
 				break;
 
-			//calculosDosPedidos.isMaisBaratoQue(pedidoAtual);
-			//calculosDosPedidos.isMaisCaroQue(pedidoAtual);
-
-			this.totalDeProdutosVendidos += pedidoAtual.getQuantidade();
 			calculosDosPedidos.totalDePedidosRealizados(pedidoAtual);
 
 			if (!categoriasProcessadas.contains(pedidoAtual.getCategoria())) {
@@ -52,17 +47,13 @@ public class RelatorioSintetico {
 		this.geraRelatorio();
 		System.out.println("#### RELATÓRIO DE VALORES TOTAIS");
 		System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", calculosDosPedidos.getTotalDePedidosRealizados());
-		System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", this.getTotalDeProdutosVendidos());
+		System.out.printf("- TOTAL DE PRODUTOS VENDIDOS: %s\n", calculosDosPedidos.getTotalDeProdutosVendidos());
 		System.out.printf("- TOTAL DE CATEGORIAS: %s\n", this.getTotalDeCategorias());
 		System.out.printf("- MONTANTE DE VENDAS: %s\n", calculosDosPedidos.valorTotalFormatado());
 		System.out.printf("- PEDIDO MAIS BARATO: %s (%s)\n", calculosDosPedidos.pedidoMaisBaratoFormatado(),
 				calculosDosPedidos.getPedidoMaisBarato().getProduto());
 		System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", calculosDosPedidos.pedidoMaisCaroQueFormatado(),
 				calculosDosPedidos.getPedidoMaisCaro().getProduto());
-	}
-
-	public int getTotalDeProdutosVendidos() {
-		return this.totalDeProdutosVendidos;
 	}
 
 	public int getTotalDeCategorias() {
--- src/main/java/br/com/alura/comex/pedido/CalculosDosPedidos.java
+++ src/main/java/br/com/alura/comex/pedido/CalculosDosPedidos.java
@@ -3,21 +3,21 @@ package br.com.alura.comex.pedido;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 import br.com.alura.comex.formatador.Formatacoes;
 
 public class CalculosDosPedidos {
-	
+
 	protected BigDecimal montanteDeVendas = BigDecimal.ZERO;
 	protected Pedido pedidoMaisBarato = null;
 	protected Pedido pedidoMaisCaro = null;
 	private int totalDePedidosRealizados = 0;
-	
+	private int totalDeProdutosVendidos = 0;
+
 	private Formatacoes formatador = new Formatacoes();
 
 	Double pedidoCaro;
-	
+
 	public boolean isMaisBaratoQue(Pedido pedidoAtual) {
 		if (this.pedidoMaisBarato == null || pedidoAtual.getPreco()
 				.multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(this.pedidoMaisBarato.getPreco()
@@ -30,14 +30,17 @@ public class CalculosDosPedidos {
 	}
 
 	public void definirMaisBarato(ArrayList<Pedido> pedidos) throws NoSuchFieldException {
-		this.pedidoMaisBarato = pedidos.stream().min(Comparator.comparing(Pedido::getPreco)).orElseThrow(NoSuchFieldException::new);
+		this.pedidoMaisBarato = pedidos.stream().min(Comparator.comparing(Pedido::getPreco))
+				.orElseThrow(NoSuchFieldException::new);
 	}
 
-	public void definirMaisCaro(ArrayList<Pedido> pedidos) throws NoSuchFieldException{
+	public void definirMaisCaro(ArrayList<Pedido> pedidos) throws NoSuchFieldException {
 
-		this.pedidoMaisCaro = pedidos.stream().max(Comparator.comparing(Pedido::getValorTotal)).orElseThrow(NoSuchFieldException::new);
+		this.pedidoMaisCaro = pedidos.stream().max(Comparator.comparing(Pedido::getValorTotal))
+				.orElseThrow(NoSuchFieldException::new);
 
 	}
+
 	public boolean isMaisCaroQue(Pedido pedidoAtual) {
 		if (pedidoMaisCaro == null
 				|| pedidoAtual.getPreco().multiply(new BigDecimal(pedidoAtual.getQuantidade())).compareTo(
@@ -48,15 +51,15 @@ public class CalculosDosPedidos {
 		}
 		return false;
 	}
-	
+
 	public String pedidoMaisBaratoFormatado() {
 		return formatador.formatarPedidoMaisBarato(this.pedidoMaisBarato);
 	}
-	
+
 	public Pedido getPedidoMaisBarato() {
 		return this.pedidoMaisBarato;
 	}
-	
+
 	public String pedidoMaisCaroQueFormatado() {
 		return formatador.formatarPedidoMaisCaro(this.pedidoMaisCaro);
 	}
@@ -70,19 +73,29 @@ public class CalculosDosPedidos {
 		return vtFormatado;
 	}
 
-	public void calcularMontante(ArrayList<Pedido> pedidos){
+	public void calcularMontante(ArrayList<Pedido> pedidos) {
 		pedidos.stream().forEach(p -> {
 			this.montanteDeVendas = this.montanteDeVendas.add(p.getPreco().multiply(new BigDecimal(p.getQuantidade())));
 		});
 	}
 
 	public int totalDePedidosRealizados(Pedido pedidoAtual) {
-		if(pedidoAtual != null)
+		if (pedidoAtual != null)
 			return this.totalDePedidosRealizados++;
 		return 0;
 	}
-	
+
 	public int getTotalDePedidosRealizados() {
 		return this.totalDePedidosRealizados;
+	}
+
+	public int getTotalDeProdutosVendidos() {
+		return this.totalDeProdutosVendidos;
+	}
+
+	public int contarTotalProdutoVendidos(ArrayList<Pedido> pedidos) {
+		int countForTotalProdutosVendidos = pedidos.stream().mapToInt(Pedido::getQuantidade).sum();
+		this.totalDeProdutosVendidos = countForTotalProdutosVendidos;
+		return this.totalDeProdutosVendidos;
 	}
 }
