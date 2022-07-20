e3b5737704c9deb2d5679aa04d7fbb471e547631
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-07-20 01:12:40 -0300

    Separacao da classe de relatorio de venda por categoria

--- src/main/java/br/com/alura/comex/relatorios/RelatorioSintetico.java
+++ src/main/java/br/com/alura/comex/relatorios/RelatorioSintetico.java
@@ -1,14 +1,14 @@
-package br.com.alura.comex;
+package br.com.alura.comex.relatorios;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.stream.Collectors;
 
+import br.com.alura.comex.ProcessadorDeCsv;
 import br.com.alura.comex.pedido.CalculosDosPedidos;
 import br.com.alura.comex.pedido.Pedido;
 
 public class RelatorioSintetico {
 
+	RelatorioVendasPorCategoria relatorioCategoria = new RelatorioVendasPorCategoria();
 	CalculosDosPedidos calculosDosPedidos = new CalculosDosPedidos();
 	private ArrayList<Pedido> pedidos = new ProcessadorDeCsv().registrarPedidos();
 
@@ -25,22 +25,6 @@ public class RelatorioSintetico {
 		calculosDosPedidos.calcularTotalDePedidosRealizados(this.pedidos);
 	}
 
-	private void geraRelatorioByCategoria() {
-		pedidos.sort((a, b) -> a.getCategoria().compareTo(b.getCategoria()));
-		pedidos.stream().collect(Collectors.groupingBy(Pedido::getCategoria, Collectors.counting()))
-				.entrySet()
-				.stream()
-				.sorted(Map.Entry.comparingByKey())
-				.filter(element -> element.getValue() >= 1).collect(Collectors.toList())
-				.forEach(v -> {
-					System.out.println("CATEGORIA: " + v.getKey());
-					System.out.println("QUANTIDADE VENDIDA: "
-							+ calculosDosPedidos.somarQuantidadePorCategoria(pedidos, v.getKey()));
-					System.out.printf("MONTANTE: %.2f \n\n",
-							calculosDosPedidos.calcularMontantePorCategoria(pedidos, v.getKey()));
-				});
-	}
-
 	public void imprimeRelatorio() throws NoSuchFieldException {
 		this.geraRelatorio();
 
@@ -55,8 +39,9 @@ public class RelatorioSintetico {
 				calculosDosPedidos.getPedidoMaisBarato().getProduto());
 		System.out.printf("- PEDIDO MAIS CARO: %s (%s)\n", calculosDosPedidos.pedidoMaisCaroQueFormatado(),
 				calculosDosPedidos.getPedidoMaisCaro().getProduto());
+		
 		System.out.println("#################");
 
-		this.geraRelatorioByCategoria();
+		relatorioCategoria.geraRelatorioByCategoria();
 	}
 }
---
+++ src/main/java/br/com/alura/comex/relatorios/RelatorioVendasPorCategoria.java
@@ -0,0 +1,31 @@
+package br.com.alura.comex.relatorios;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import br.com.alura.comex.ProcessadorDeCsv;
+import br.com.alura.comex.pedido.CalculosDosPedidos;
+import br.com.alura.comex.pedido.Pedido;
+
+public class RelatorioVendasPorCategoria {
+	
+	CalculosDosPedidos calculosDosPedidos = new CalculosDosPedidos();
+	private ArrayList<Pedido> pedidos = new ProcessadorDeCsv().registrarPedidos();
+
+	public RelatorioVendasPorCategoria() {
+	}
+	
+	public void geraRelatorioByCategoria() {
+		pedidos.sort((a, b) -> a.getCategoria().compareTo(b.getCategoria()));
+		pedidos.stream().collect(Collectors.groupingBy(Pedido::getCategoria, Collectors.counting())).entrySet().stream()
+				.sorted(Map.Entry.comparingByKey()).filter(element -> element.getValue() >= 1)
+				.collect(Collectors.toList()).forEach(v -> {
+					System.out.println("CATEGORIA: " + v.getKey());
+					System.out.println("QUANTIDADE VENDIDA: "
+							+ calculosDosPedidos.somarQuantidadePorCategoria(pedidos, v.getKey()));
+					System.out.printf("MONTANTE: %.2f \n\n",
+							calculosDosPedidos.calcularMontantePorCategoria(pedidos, v.getKey()));
+				});
+	}
+}
