52bb748c443a2266f065da64bc58343f91bbb6fc
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-07-27 19:54:04 -0300

    Fechamento cards  semana 1, 2 e 3

--- src/main/java/br/com/alura/comex/relatorios/RelatorioSintetico.java
+++ src/main/java/br/com/alura/comex/relatorios/RelatorioSintetico.java
@@ -62,12 +62,6 @@ public class RelatorioSintetico {
 		System.out.println("######## Relatorio produto mais caro de cada categoria #########\n");
 		
 		relatorioCategoria.apresentaRelatorioProdutoMaisCaro();
-		
-		System.out.println("######## TESTE #########\n");
-		
-		relatorioCategoria.apresentaRelatorioProdutoMaisCaro_umPedido();
-		System.out.println("######## TESTE #########\n");
-		relatorioCategoria.apresentaRelatorioComClientesEProduto();
 			
 	}
 }
